88722aacd5fbeb7b6e53772c8a92dd6c1c547b16
ozairkhaliq <ozair.khaliq@gmail.com>
2024-07-15 21:03:50 -0500

    7/15

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,79 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList(); 
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+		public IActionResult Create(Category obj)
+		{
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Name cannot be the same as the Display Order");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "The Name cannot be 'name'");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+				return RedirectToAction("Index", "Category");
+			}
+            return View();
+		}
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryById = _db.Categories.Find(id);
+
+            if (categoryById == null)
+            {
+                return NotFound();
+            }
+			return View(categoryById);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The Name cannot be the same as the Display Order");
+			}
+			if (obj.Name.ToLower() == "test")
+			{
+				ModelState.AddModelError("", "The Name cannot be 'name'");
+			}
+
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
+	}
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+/*
+ columns in category table
+prop id - int - primary key
+prop name - string
+prop displayorder - int
+required data annotation for name
+ */
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}

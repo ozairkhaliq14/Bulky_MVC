563a8c37e152dee290085182b875ce73fa0f4057
ozairkhaliq <ozair.khaliq@gmail.com>
2024-07-20 15:13:42 -0500

    ForeignKey and Image Url added

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -32,7 +32,9 @@ namespace BulkyBookWeb.DataAccess.Data
                     ListPrice = 99,
                     Price = 90,
                     Price50 = 85,
-                    Price100 = 80
+                    Price100 = 80,
+                    CategoryId = 1,
+                    ImageUrl = ""
                 },
                 new Product
                 {
@@ -44,8 +46,10 @@ namespace BulkyBookWeb.DataAccess.Data
                     ListPrice = 40,
                     Price = 30,
                     Price50 = 25,
-                    Price100 = 20
-                },
+                    Price100 = 20,
+					CategoryId = 1,
+					ImageUrl = ""
+				},
                 new Product
                 {
                     Id = 3,
@@ -56,8 +60,10 @@ namespace BulkyBookWeb.DataAccess.Data
                     ListPrice = 55,
                     Price = 50,
                     Price50 = 40,
-                    Price100 = 35
-                },
+                    Price100 = 35,
+					CategoryId = 1,
+					ImageUrl = ""
+				},
                 new Product
                 {
                     Id = 4,
@@ -68,8 +74,10 @@ namespace BulkyBookWeb.DataAccess.Data
                     ListPrice = 70,
                     Price = 65,
                     Price50 = 60,
-                    Price100 = 55
-                },
+                    Price100 = 55,
+					CategoryId = 2,
+					ImageUrl = ""
+				},
                 new Product
                 {
                     Id = 5,
@@ -80,8 +88,10 @@ namespace BulkyBookWeb.DataAccess.Data
                     ListPrice = 30,
                     Price = 27,
                     Price50 = 25,
-                    Price100 = 20
-                },
+                    Price100 = 20,
+					CategoryId = 3,
+					ImageUrl = ""
+				},
                 new Product
                 {
                     Id = 6,
@@ -92,8 +102,10 @@ namespace BulkyBookWeb.DataAccess.Data
                     ListPrice = 25,
                     Price = 23,
                     Price50 = 22,
-                    Price100 = 20
-                }
+                    Price100 = 20,
+					CategoryId = 3,
+					ImageUrl = ""
+				}
                 );
         }
 
--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,6 +44,12 @@ namespace BulkyBook.Models
         [Range(0, 1000)]
         [DisplayName("Price for Quantity 100+ books")]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
 
     }
 }
